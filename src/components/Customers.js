bc9c0dc13c65cf834a94d11c243e3000701ac3bf
Victor Ajagunna <victor-ajagunna@hotmail.com>
2022-10-16 17:30:52 +0200

    Customers's data commit

--- src/components/Customers.js
+++ src/components/Customers.js
@@ -1,9 +1,705 @@
-import React from 'react';
+export const Customers = [
+    {
+        "id": 1,
+        "fullname": "Gearard Endon",
+        "gender": "Male",
+        "email": "gendon0@phoca.cz",
+        "phone": "+1-713-633-0738",
+        "joined": "3/23/2022"
+    }, {
+    "id": 2,
+    "fullname": "Doralynn Tapping",
+    "gender": "Female",
+    "email": "dtapping1@naver.com",
+    "phone": "+372-771-685-3418",
+    "joined": "12/10/2021"
+    }, {
+    "id": 3,
+    "fullname": "Wash Corke",
+    "gender": "Male",
+    "email": "wcorke2@furl.net",
+    "phone": "+420-517-415-3054",
+    "joined": "2/25/2022"
+    }, {
+    "id": 4,
+    "fullname": "Thom Delacour",
+    "gender": "Male",
+    "email": "tdelacour3@chronoengine.com",
+    "phone": "+86-950-143-9929",
+    "joined": "8/18/2022"
+    }, {
+    "id": 5,
+    "fullname": "Nikola Lalonde",
+    "gender": "Male",
+    "email": "nlalonde4@devhub.com",
+    "phone": "+66-476-944-3115",
+    "joined": "3/19/2022"
+    }, {
+    "id": 6,
+    "fullname": "Bernie Dakin",
+    "gender": "Female",
+    "email": "bdakin5@nymag.com",
+    "phone": "+1-241-198-2064",
+    "joined": "7/3/2022"
+    }, {
+    "id": 7,
+    "fullname": "Lynnette Tabram",
+    "gender": "Female",
+    "email": "ltabram6@blog.com",
+    "phone": "+30-511-796-1482",
+    "joined": "1/7/2022"
+    }, {
+    "id": 8,
+    "fullname": "Stearn Fishley",
+    "gender": "Non-binary",
+    "email": "sfishley7@opera.com",
+    "phone": "+86-545-263-4855",
+    "joined": "6/26/2022"
+    }, {
+    "id": 9,
+    "fullname": "Lucilia Varren",
+    "gender": "Female",
+    "email": "lvarren8@posterous.com",
+    "phone": "+355-213-276-7726",
+    "joined": "10/7/2022"
+    }, {
+    "id": 10,
+    "fullname": "Sauveur Moon",
+    "gender": "Genderfluid",
+    "email": "smoon9@ow.ly",
+    "phone": "+51-195-462-3341",
+    "joined": "10/19/2021"
+    }, {
+    "id": 11,
+    "fullname": "Germaine O'Ruane",
+    "gender": "Male",
+    "email": "goruanea@photobucket.com",
+    "phone": "+251-302-603-7633",
+    "joined": "12/8/2021"
+    }, {
+    "id": 12,
+    "fullname": "Amata Sexti",
+    "gender": "Female",
+    "email": "asextib@google.de",
+    "phone": "+48-135-762-5279",
+    "joined": "12/25/2021"
+    }, {
+    "id": 13,
+    "fullname": "Inessa Daubeny",
+    "gender": "Female",
+    "email": "idaubenyc@bizjournals.com",
+    "phone": "+86-496-660-4399",
+    "joined": "6/8/2022"
+    }, {
+    "id": 14,
+    "fullname": "Arabela Gostage",
+    "gender": "Female",
+    "email": "agostaged@mozilla.org",
+    "phone": "+880-748-789-4583",
+    "joined": "12/29/2021"
+    }, {
+    "id": 15,
+    "fullname": "Loralyn Maudling",
+    "gender": "Female",
+    "email": "lmaudlinge@mysql.com",
+    "phone": "+48-857-237-8958",
+    "joined": "9/27/2022"
+    }, {
+    "id": 16,
+    "fullname": "Pattin Gerrill",
+    "gender": "Male",
+    "email": "pgerrillf@cnn.com",
+    "phone": "+54-209-338-5647",
+    "joined": "7/18/2022"
+    }, {
+    "id": 17,
+    "fullname": "Flore Sidlow",
+    "gender": "Female",
+    "email": "fsidlowg@flavors.me",
+    "phone": "+86-457-516-4260",
+    "joined": "12/29/2021"
+    }, {
+    "id": 18,
+    "fullname": "Elvyn Chancellor",
+    "gender": "Non-binary",
+    "email": "echancellorh@addtoany.com",
+    "phone": "+234-275-163-5822",
+    "joined": "12/11/2021"
+    }, {
+    "id": 19,
+    "fullname": "Sargent Muldrew",
+    "gender": "Genderqueer",
+    "email": "smuldrewi@forbes.com",
+    "phone": "+86-237-230-7411",
+    "joined": "9/9/2022"
+    }, {
+    "id": 20,
+    "fullname": "Elenore Letchford",
+    "gender": "Female",
+    "email": "eletchfordj@prnewswire.com",
+    "phone": "+351-170-664-6060",
+    "joined": "12/25/2021"
+    }, {
+    "id": 21,
+    "fullname": "Tripp Macartney",
+    "gender": "Male",
+    "email": "tmacartneyk@ox.ac.uk",
+    "phone": "+55-609-739-1991",
+    "joined": "6/14/2022"
+    }, {
+    "id": 22,
+    "fullname": "Rees Donnan",
+    "gender": "Male",
+    "email": "rdonnanl@miitbeian.gov.cn",
+    "phone": "+86-440-674-5125",
+    "joined": "1/8/2022"
+    }, {
+    "id": 23,
+    "fullname": "Federico Stroud",
+    "gender": "Male",
+    "email": "fstroudm@artisteer.com",
+    "phone": "+55-734-697-8753",
+    "joined": "10/9/2022"
+    }, {
+    "id": 24,
+    "fullname": "Alexia Cuseck",
+    "gender": "Female",
+    "email": "acuseckn@cargocollective.com",
+    "phone": "+86-992-432-9212",
+    "joined": "7/31/2022"
+    }, {
+    "id": 25,
+    "fullname": "Christine Cooley",
+    "gender": "Female",
+    "email": "ccooleyo@blinklist.com",
+    "phone": "+33-289-217-1793",
+    "joined": "9/7/2022"
+    }, {
+    "id": 26,
+    "fullname": "Deeyn Fox",
+    "gender": "Female",
+    "email": "dfoxp@cloudflare.com",
+    "phone": "+1-865-470-4052",
+    "joined": "5/25/2022"
+    }, {
+    "id": 27,
+    "fullname": "Rubetta Menchenton",
+    "gender": "Female",
+    "email": "rmenchentonq@alibaba.com",
+    "phone": "+58-583-357-7455",
+    "joined": "3/23/2022"
+    }, {
+    "id": 28,
+    "fullname": "Guido Althrope",
+    "gender": "Male",
+    "email": "galthroper@europa.eu",
+    "phone": "+86-984-977-3034",
+    "joined": "12/11/2021"
+    }, {
+    "id": 29,
+    "fullname": "Sophey Bowler",
+    "gender": "Female",
+    "email": "sbowlers@wikimedia.org",
+    "phone": "+998-539-142-4745",
+    "joined": "12/22/2021"
+    }, {
+    "id": 30,
+    "fullname": "Leontyne Dennick",
+    "gender": "Agender",
+    "email": "ldennickt@ebay.co.uk",
+    "phone": "+386-122-669-7230",
+    "joined": "4/30/2022"
+    }, {
+    "id": 31,
+    "fullname": "Gifford Stroban",
+    "gender": "Male",
+    "email": "gstrobanu@nsw.gov.au",
+    "phone": "+55-656-357-7865",
+    "joined": "7/8/2022"
+    }, {
+    "id": 32,
+    "fullname": "Odelinda Dingley",
+    "gender": "Female",
+    "email": "odingleyv@google.co.uk",
+    "phone": "+98-325-102-1993",
+    "joined": "10/8/2022"
+    }, {
+    "id": 33,
+    "fullname": "Dulcea Kettoe",
+    "gender": "Female",
+    "email": "dkettoew@prnewswire.com",
+    "phone": "+359-969-235-4414",
+    "joined": "11/2/2021"
+    }, {
+    "id": 34,
+    "fullname": "Feliks Chadbourne",
+    "gender": "Male",
+    "email": "fchadbournex@sitemeter.com",
+    "phone": "+53-856-133-4069",
+    "joined": "6/2/2022"
+    }, {
+    "id": 35,
+    "fullname": "Etan Telford",
+    "gender": "Male",
+    "email": "etelfordy@simplemachines.org",
+    "phone": "+963-952-692-5207",
+    "joined": "3/14/2022"
+    }, {
+    "id": 36,
+    "fullname": "Siffre Armour",
+    "gender": "Male",
+    "email": "sarmourz@toplist.cz",
+    "phone": "+381-499-362-5966",
+    "joined": "7/9/2022"
+    }, {
+    "id": 37,
+    "fullname": "Mollee Dedden",
+    "gender": "Female",
+    "email": "mdedden10@shutterfly.com",
+    "phone": "+86-962-836-2325",
+    "joined": "1/18/2022"
+    }, {
+    "id": 38,
+    "fullname": "Marguerite Buzine",
+    "gender": "Female",
+    "email": "mbuzine11@sina.com.cn",
+    "phone": "+254-400-740-0511",
+    "joined": "3/30/2022"
+    }, {
+    "id": 39,
+    "fullname": "Linea Looks",
+    "gender": "Female",
+    "email": "llooks12@bloglines.com",
+    "phone": "+996-544-434-3785",
+    "joined": "8/6/2022"
+    }, {
+    "id": 40,
+    "fullname": "Stanford Dunkerk",
+    "gender": "Male",
+    "email": "sdunkerk13@abc.net.au",
+    "phone": "+880-455-636-5496",
+    "joined": "9/25/2022"
+    }, {
+    "id": 41,
+    "fullname": "Catherine Pellamont",
+    "gender": "Female",
+    "email": "cpellamont14@cbslocal.com",
+    "phone": "+86-164-114-7553",
+    "joined": "3/10/2022"
+    }, {
+    "id": 42,
+    "fullname": "Lena McRuvie",
+    "gender": "Female",
+    "email": "lmcruvie15@irs.gov",
+    "phone": "+996-583-848-8793",
+    "joined": "10/5/2022"
+    }, {
+    "id": 43,
+    "fullname": "Alexandro Lowin",
+    "gender": "Male",
+    "email": "alowin16@ucsd.edu",
+    "phone": "+351-880-386-5239",
+    "joined": "3/19/2022"
+    }, {
+    "id": 44,
+    "fullname": "Lil Cunnington",
+    "gender": "Genderqueer",
+    "email": "lcunnington17@topsy.com",
+    "phone": "+66-901-514-3223",
+    "joined": "10/22/2021"
+    }, {
+    "id": 45,
+    "fullname": "Romonda Mersey",
+    "gender": "Female",
+    "email": "rmersey18@delicious.com",
+    "phone": "+86-835-521-9364",
+    "joined": "1/1/2022"
+    }, {
+    "id": 46,
+    "fullname": "Loleta Kike",
+    "gender": "Female",
+    "email": "lkike19@i2i.jp",
+    "phone": "+81-104-522-5894",
+    "joined": "11/16/2021"
+    }, {
+    "id": 47,
+    "fullname": "Coleman Gartrell",
+    "gender": "Male",
+    "email": "cgartrell1a@bigcartel.com",
+    "phone": "+48-470-772-5434",
+    "joined": "6/23/2022"
+    }, {
+    "id": 48,
+    "fullname": "Ellissa Casley",
+    "gender": "Female",
+    "email": "ecasley1b@wp.com",
+    "phone": "+351-816-326-3888",
+    "joined": "8/8/2022"
+    }, {
+    "id": 49,
+    "fullname": "Chester Styles",
+    "gender": "Male",
+    "email": "cstyles1c@amazon.com",
+    "phone": "+966-904-790-9194",
+    "joined": "12/21/2021"
+    }, {
+    "id": 50,
+    "fullname": "Eyde Scade",
+    "gender": "Female",
+    "email": "escade1d@mayoclinic.com",
+    "phone": "+57-613-236-7818",
+    "joined": "10/17/2021"
+    }, {
+    "id": 51,
+    "fullname": "Cam Klein",
+    "gender": "Female",
+    "email": "cklein1e@woothemes.com",
+    "phone": "+86-812-158-7725",
+    "joined": "1/31/2022"
+    }, {
+    "id": 52,
+    "fullname": "Chaddy Hopkyns",
+    "gender": "Male",
+    "email": "chopkyns1f@intel.com",
+    "phone": "+62-154-298-3996",
+    "joined": "7/6/2022"
+    }, {
+    "id": 53,
+    "fullname": "Tonnie Goodliff",
+    "gender": "Male",
+    "email": "tgoodliff1g@examiner.com",
+    "phone": "+48-739-725-5366",
+    "joined": "7/17/2022"
+    }, {
+    "id": 54,
+    "fullname": "Tracey Jahner",
+    "gender": "Male",
+    "email": "tjahner1h@nymag.com",
+    "phone": "+86-682-727-7370",
+    "joined": "7/10/2022"
+    }, {
+    "id": 55,
+    "fullname": "Ibby Dearnly",
+    "gender": "Female",
+    "email": "idearnly1i@bluehost.com",
+    "phone": "+7-370-158-0026",
+    "joined": "1/12/2022"
+    }, {
+    "id": 56,
+    "fullname": "Porty Deguara",
+    "gender": "Male",
+    "email": "pdeguara1j@xrea.com",
+    "phone": "+46-270-658-2152",
+    "joined": "4/10/2022"
+    }, {
+    "id": 57,
+    "fullname": "Egon Terron",
+    "gender": "Genderqueer",
+    "email": "eterron1k@wix.com",
+    "phone": "+57-816-759-8131",
+    "joined": "10/31/2021"
+    }, {
+    "id": 58,
+    "fullname": "Curr Tant",
+    "gender": "Male",
+    "email": "ctant1l@cam.ac.uk",
+    "phone": "+351-840-574-6080",
+    "joined": "8/13/2022"
+    }, {
+    "id": 59,
+    "fullname": "Pincus Legister",
+    "gender": "Male",
+    "email": "plegister1m@usatoday.com",
+    "phone": "+248-501-156-6465",
+    "joined": "11/11/2021"
+    }, {
+    "id": 60,
+    "fullname": "Jean Barlas",
+    "gender": "Female",
+    "email": "jbarlas1n@pen.io",
+    "phone": "+220-926-398-1020",
+    "joined": "7/6/2022"
+    }, {
+    "id": 61,
+    "fullname": "Dominic Tomaszewski",
+    "gender": "Male",
+    "email": "dtomaszewski1o@yelp.com",
+    "phone": "+389-311-612-9270",
+    "joined": "3/25/2022"
+    }, {
+    "id": 62,
+    "fullname": "Essie Linton",
+    "gender": "Female",
+    "email": "elinton1p@xing.com",
+    "phone": "+7-645-352-0334",
+    "joined": "5/18/2022"
+    }, {
+    "id": 63,
+    "fullname": "Lorilee Prior",
+    "gender": "Female",
+    "email": "lprior1q@miitbeian.gov.cn",
+    "phone": "+1-190-223-3524",
+    "joined": "5/31/2022"
+    }, {
+    "id": 64,
+    "fullname": "Maddy Bentote",
+    "gender": "Polygender",
+    "email": "mbentote1r@ihg.com",
+    "phone": "+86-553-461-1332",
+    "joined": "9/28/2022"
+    }, {
+    "id": 65,
+    "fullname": "Darb Scade",
+    "gender": "Male",
+    "email": "dscade1s@ifeng.com",
+    "phone": "+62-574-368-5414",
+    "joined": "4/21/2022"
+    }, {
+    "id": 66,
+    "fullname": "Josefina Mallan",
+    "gender": "Female",
+    "email": "jmallan1t@fda.gov",
+    "phone": "+234-430-679-7808",
+    "joined": "5/24/2022"
+    }, {
+    "id": 67,
+    "fullname": "Rube McKibbin",
+    "gender": "Male",
+    "email": "rmckibbin1u@icq.com",
+    "phone": "+62-965-916-0973",
+    "joined": "9/19/2022"
+    }, {
+    "id": 68,
+    "fullname": "Olivero Bricksey",
+    "gender": "Male",
+    "email": "obricksey1v@moonfruit.com",
+    "phone": "+381-680-691-3446",
+    "joined": "4/21/2022"
+    }, {
+    "id": 69,
+    "fullname": "Haleigh Juszczyk",
+    "gender": "Male",
+    "email": "hjuszczyk1w@lulu.com",
+    "phone": "+57-367-607-3820",
+    "joined": "6/18/2022"
+    }, {
+    "id": 70,
+    "fullname": "Nickolaus Tesmond",
+    "gender": "Male",
+    "email": "ntesmond1x@ed.gov",
+    "phone": "+92-548-144-0372",
+    "joined": "10/6/2022"
+    }, {
+    "id": 71,
+    "fullname": "Bartram Southard",
+    "gender": "Male",
+    "email": "bsouthard1y@zdnet.com",
+    "phone": "+223-697-321-8934",
+    "joined": "2/16/2022"
+    }, {
+    "id": 72,
+    "fullname": "Brennan MacNeill",
+    "gender": "Male",
+    "email": "bmacneill1z@constantcontact.com",
+    "phone": "+380-991-532-2048",
+    "joined": "7/8/2022"
+    }, {
+    "id": 73,
+    "fullname": "Weidar Kaygill",
+    "gender": "Male",
+    "email": "wkaygill20@statcounter.com",
+    "phone": "+62-281-906-2883",
+    "joined": "10/5/2022"
+    }, {
+    "id": 74,
+    "fullname": "Cathy Beesley",
+    "gender": "Female",
+    "email": "cbeesley21@sina.com.cn",
+    "phone": "+7-467-829-7259",
+    "joined": "3/24/2022"
+    }, {
+    "id": 75,
+    "fullname": "Eugene Dumbare",
+    "gender": "Male",
+    "email": "edumbare22@epa.gov",
+    "phone": "+33-270-155-9230",
+    "joined": "6/27/2022"
+    }, {
+    "id": 76,
+    "fullname": "Ase Mordy",
+    "gender": "Male",
+    "email": "amordy23@yale.edu",
+    "phone": "+46-970-745-1035",
+    "joined": "3/27/2022"
+    }, {
+    "id": 77,
+    "fullname": "Pattie Kort",
+    "gender": "Female",
+    "email": "pkort24@wikia.com",
+    "phone": "+670-283-227-9238",
+    "joined": "11/1/2021"
+    }, {
+    "id": 78,
+    "fullname": "Justin Probart",
+    "gender": "Male",
+    "email": "jprobart25@patch.com",
+    "phone": "+240-263-204-2410",
+    "joined": "10/4/2022"
+    }, {
+    "id": 79,
+    "fullname": "Kate Ruprechter",
+    "gender": "Female",
+    "email": "kruprechter26@admin.ch",
+    "phone": "+48-775-450-0010",
+    "joined": "3/3/2022"
+    }, {
+    "id": 80,
+    "fullname": "Lennard Chesterfield",
+    "gender": "Male",
+    "email": "lchesterfield27@wufoo.com",
+    "phone": "+261-647-944-9115",
+    "joined": "2/19/2022"
+    }, {
+    "id": 81,
+    "fullname": "Margaretta Abate",
+    "gender": "Female",
+    "email": "mabate28@miitbeian.gov.cn",
+    "phone": "+228-790-540-8456",
+    "joined": "11/28/2021"
+    }, {
+    "id": 82,
+    "fullname": "Erl Granger",
+    "gender": "Male",
+    "email": "egranger29@51.la",
+    "phone": "+62-104-689-1729",
+    "joined": "3/25/2022"
+    }, {
+    "id": 83,
+    "fullname": "Zacharie Dyster",
+    "gender": "Male",
+    "email": "zdyster2a@phpbb.com",
+    "phone": "+33-504-732-2376",
+    "joined": "12/4/2021"
+    }, {
+    "id": 84,
+    "fullname": "Woodman Reedman",
+    "gender": "Male",
+    "email": "wreedman2b@symantec.com",
+    "phone": "+86-778-612-5013",
+    "joined": "3/5/2022"
+    }, {
+    "id": 85,
+    "fullname": "Gwynne Eburah",
+    "gender": "Female",
+    "email": "geburah2c@nbcnews.com",
+    "phone": "+48-812-567-7184",
+    "joined": "3/21/2022"
+    }, {
+    "id": 86,
+    "fullname": "Codee Bullocke",
+    "gender": "Female",
+    "email": "cbullocke2d@biblegateway.com",
+    "phone": "+86-834-771-1642",
+    "joined": "4/2/2022"
+    }, {
+    "id": 87,
+    "fullname": "Jaquenetta Beringer",
+    "gender": "Female",
+    "email": "jberinger2e@netlog.com",
+    "phone": "+93-347-869-3473",
+    "joined": "1/7/2022"
+    }, {
+    "id": 88,
+    "fullname": "Carley Buffy",
+    "gender": "Female",
+    "email": "cbuffy2f@vinaora.com",
+    "phone": "+51-603-822-3713",
+    "joined": "3/7/2022"
+    }, {
+    "id": 89,
+    "fullname": "Genevieve Ebbage",
+    "gender": "Female",
+    "email": "gebbage2g@symantec.com",
+    "phone": "+58-266-579-2076",
+    "joined": "2/7/2022"
+    }, {
+    "id": 90,
+    "fullname": "Gonzales Blackman",
+    "gender": "Male",
+    "email": "gblackman2h@bizjournals.com",
+    "phone": "+62-478-528-8871",
+    "joined": "2/19/2022"
+    }, {
+    "id": 91,
+    "fullname": "Freddi Scrammage",
+    "gender": "Female",
+    "email": "fscrammage2i@mit.edu",
+    "phone": "+98-670-540-2008",
+    "joined": "1/15/2022"
+    }, {
+    "id": 92,
+    "fullname": "Rodie Pahl",
+    "gender": "Female",
+    "email": "rpahl2j@aol.com",
+    "phone": "+95-353-986-5182",
+    "joined": "9/22/2022"
+    }, {
+    "id": 93,
+    "fullname": "Tedd Lucks",
+    "gender": "Polygender",
+    "email": "tlucks2k@cornell.edu",
+    "phone": "+386-968-250-1681",
+    "joined": "1/25/2022"
+    }, {
+    "id": 94,
+    "fullname": "Umberto Dowthwaite",
+    "gender": "Polygender",
+    "email": "udowthwaite2l@purevolume.com",
+    "phone": "+86-438-452-3715",
+    "joined": "8/7/2022"
+    }, {
+    "id": 95,
+    "fullname": "Fara Crummie",
+    "gender": "Female",
+    "email": "fcrummie2m@list-manage.com",
+    "phone": "+54-991-745-6218",
+    "joined": "5/5/2022"
+    }, {
+    "id": 96,
+    "fullname": "Sybila Barke",
+    "gender": "Female",
+    "email": "sbarke2n@newsvine.com",
+    "phone": "+7-273-251-5380",
+    "joined": "6/23/2022"
+    }, {
+    "id": 97,
+    "fullname": "Pippa Thomel",
+    "gender": "Female",
+    "email": "pthomel2o@cyberchimps.com",
+    "phone": "+49-556-472-2014",
+    "joined": "2/17/2022"
+    }, {
+    "id": 98,
+    "fullname": "Borg Crevy",
+    "gender": "Male",
+    "email": "bcrevy2p@intel.com",
+    "phone": "+62-255-600-1710",
+    "joined": "9/18/2022"
+    }, {
+    "id": 99,
+    "fullname": "Yelena Lowe",
+    "gender": "Female",
+    "email": "ylowe2q@privacy.gov.au",
+    "phone": "+7-341-724-1184",
+    "joined": "10/18/2021"
+    }, {
+    "id": 100,
+    "fullname": "Marley Tradewell",
+    "gender": "Female",
+    "email": "mtradewell2r@illinois.edu",
+    "phone": "+55-182-923-2921",
+    "joined": "8/10/2022"
+    }
+]
 
-const Customers = () => {
-  return (
-    <div>Customers</div>
-  )
-}
-
-export default Customers;
+//"https://api.mockaroo.com/api/3f462750?count=1000&key=ae62a510" > "Customers.json"
